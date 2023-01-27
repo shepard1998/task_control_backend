6314fdba5269395e49e4d5b5cfd3e6ad268c5559
Kevin <kevinfdez41@gmail.com>
2023-01-27 14:49:33 -0600

    fix: E2E test is now more complete

--- src/test/java/com/technicaltest/taskcontrol/StepDefinitions.java
+++ src/test/java/com/technicaltest/taskcontrol/StepDefinitions.java
@@ -1,21 +1,20 @@
 package com.technicaltest.taskcontrol;
 
+import com.technicaltest.taskcontrol.tag.Tag;
 import com.technicaltest.taskcontrol.task.Task;
 import groovy.util.logging.Log4j2;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,40 +33,63 @@ public class StepDefinitions
     private static Response response;
     private static String jsonString;
     private static RestTemplate restTemplate = new RestTemplate();
+    private static Long taskId;
+    private static Long tagId_1;
+    private static Long tagId_2;
 
-    @Given("A list of tasks is available")
-    public void a_list_of_tasks_is_available() {
-        /*RestAssured.baseURI = BASE_URL;
-        RequestSpecification request = RestAssured.given();
-        response = request.get("api/v1/task");
-
-        jsonString = response.asString();
-        List<Map<String, String>> tasks = JsonPath.from(jsonString).get("task");
-        Assert.assertTrue(tasks.size() > 0);
-
-        String id = tasks.get(0).get("isbn");*/
-
+    @Given("A list of tasks is empty")
+    public void a_list_of_tasks_is_empty()
+    {
         String url = BASE_URL + ":" + port + "/api/v1/task";
         List<Task> tasks = restTemplate.getForObject(url, List.class);
         assertTrue(tasks.isEmpty());
-
     }
     @When("I add a new task")
     public void i_add_a_new_task() {
-        // Write code here that turns the phrase above into concrete actions
         String url = BASE_URL + ":" + port + "/api/v1/task";
         Task newTask = new Task();
         Task task = restTemplate.postForObject(url, newTask, Task.class);
+        taskId = task.getId();
         assertNotNull(task);
-
     }
     @Then("The task is added to the list and stored in database")
     public void the_task_is_added_to_the_list_and_stored_in_database() {
-        // Write code here that turns the phrase above into concrete actions
         String url = BASE_URL + ":" + port + "/api/v1/task";
         List<Task> tasks = restTemplate.getForObject(url, List.class);
         assertTrue(!tasks.isEmpty());
+    }
+    @Then("new Tag list is created")
+    public void new_tag_list_is_created() {
+        String url = BASE_URL + ":" + port + "/api/v1/tag";
+        Tag tag1 = new Tag();
+        Tag tag2 = new Tag();
 
+        tag1.setText("Tag list ");
+        tag2.setText("#TEST");
+
+        Tag tag = restTemplate.postForObject(url, tag1, Tag.class);
+        assertNotNull(tag);
+        tagId_1 = tag.getId();
+
+        tag = restTemplate.postForObject(url, tag2, Tag.class);
+        assertNotNull(tag);
+        tagId_2 = tag.getId();
+    }
+    @Then("Tag list is assigned to the Task")
+    public void tag_list_is_assigned_to_the_task() {
+        String url = BASE_URL + ":" + port + "/api/v1/tag/{tagId}/task/{taskId}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+
+        ResponseEntity<Tag> responseEntity =
+                restTemplate.exchange(url, HttpMethod.PUT, entity, Tag.class, tagId_1, taskId);
+        ResponseEntity<Tag> responseEntity_2 =
+                restTemplate.exchange(url, HttpMethod.PUT, entity, Tag.class, tagId_2, taskId);
+
+        assertNotNull(responseEntity.getBody());
+        assertNotNull(responseEntity_2.getBody());
     }
 
 }
--- src/test/resources/features/TestingCRUD.feature
+++ src/test/resources/features/TestingCRUD.feature
@@ -2,6 +2,8 @@ Feature: Insert Task
   Description: The purpose of these test are cover CRUD functionalities
 
   Scenario: Users can add new tasks
-    Given A list of tasks is available
+    Given A list of tasks is empty
     When I add a new task
     Then The task is added to the list and stored in database
+    And new Tag list is created
+    And Tag list is assigned to the Task
